7c9b17c4bd339bee0816c6ab3619d05422cd6ee1
Felipe Alamos <felipealamos@gmail.com>
2023-02-03 13:37:26 -0500

    CRUD finished

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -1,3 +1,4 @@
+import * as functions from "firebase-functions";
 import { Response } from 'express'
 import { db } from './config/firebase'
 
@@ -11,7 +12,9 @@ type Request = {
   params: { entryId: string }
 }
 
+// CREATE
 const addEntry = async (req: Request, res: Response) => {
+  
   let { title, text } = req.body
   title = (typeof title === 'undefined') ? 'default_title' : title;
   text = (typeof text === 'undefined') ? 'default_text' : text;
@@ -36,6 +39,7 @@ const addEntry = async (req: Request, res: Response) => {
   }
 }
 
+// READ
 const getAllEntries = async (req: Request, res: Response) => {
   try {
     const allEntries: EntryType[] = []
@@ -45,4 +49,109 @@ const getAllEntries = async (req: Request, res: Response) => {
   } catch(error) { return res.status(500).json(error.message) }
 }
 
-export { addEntry, getAllEntries }
+const getEntry = async (req: Request, res: Response) => {
+
+  const { entryId } = req.params
+
+  try {
+    const entry = db.collection('entries').doc(entryId)
+    const entryData = (await entry.get()).data()    
+
+    // Validate that entry exists
+    if (typeof entryData === 'undefined') {
+      return res.status(400).json({
+        status: 'error',
+        message: 'entry does not exist',
+        entry_id: entryId
+        }
+      )  
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'got entry',
+      data: entryData
+    })
+  }
+  catch(error) { return res.status(500).json(error.message) }
+}
+
+
+
+// UPDATE
+const updateEntry = async (req: Request, res: Response) => {
+  const { body: { text, title }, params: { entryId } } = req
+
+  try {
+    const entry = db.collection('entries').doc(entryId)
+    const currentData = (await entry.get()).data()
+
+    // Validate that entry exists
+    if (typeof currentData === 'undefined') {
+      return res.status(400).json({
+        status: 'error',
+        message: 'entry does not exist',
+        entry_id: entryId
+        }
+      )  
+    }
+    const entryObject = {
+      title: title || currentData.title,
+      text: text || currentData.text,
+    }
+
+    await entry.set(entryObject).catch(error => {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message
+      })
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'entry updated successfully',
+      data: entryObject
+    })
+  }
+  catch(error) { return res.status(500).json(error.message) }
+}
+
+
+// DELETE
+const deleteEntry = async (req: Request, res: Response) => {
+  const { entryId } = req.params
+
+  try {
+    
+    const entry = db.collection('entries').doc(entryId)
+
+    // Validate that entry exists
+    const entryData = (await entry.get()).data()    
+    if (typeof entryData === 'undefined') {
+      return res.status(400).json({
+        status: 'error',
+        message: 'entry does not exist',
+        entry_id: entryId
+        }
+      )  
+    }
+
+    // Delete entry
+    await entry.delete().catch(error => {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+        entry_id: entryId
+      })
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'entry deleted successfully',
+      entry_id: entryId
+    })
+  }
+  catch(error) { return res.status(500).json(error.message) }
+}
+
+export { addEntry, getEntry, getAllEntries, updateEntry, deleteEntry }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import * as express from 'express'
 
 
-import { addEntry, getAllEntries } from './entryController'
+import { addEntry, getEntry, getAllEntries, updateEntry, deleteEntry } from './entryController'
 
 
 const app = express()
@@ -20,6 +20,9 @@ app.get('/health', (req, res) => {
 
 app.post('/entries', addEntry)
 app.get('/entries', getAllEntries)
+app.get('/entries/:entryId', getEntry)
+app.patch('/entries/:entryId', updateEntry)
+app.delete('/entries/:entryId', deleteEntry)
 
 
 exports.app = functions.https.onRequest(app)
